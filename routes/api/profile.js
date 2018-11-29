c172d2f94406f19b8860ba4a37ffda38d255be83
Robert Farb <farb.robert@gmail.com>
2018-11-29 12:04:26 -0800

    Add route to delete experience from profile experience array

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -193,5 +193,25 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
     });
 });
 
+// @route DELETE api/profile/experience/:exp_id
+// @desc Delete Experience from profile
+// @access Private
+router.post('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      // Get index of experience to remove
+      const removeIdx = profile.experience
+        .map(item => item.id)
+        .indexOf(req.params.exp_id);
+
+      // Remove that experience from the array
+      profile.experience.splice(removeIdx, 1);
+
+      profile.save().then(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 
 module.exports = router;
