ba6b83d517943da7f56e1f4042d53356c7d568c4
Robert Farb <farb.robert@gmail.com>
2018-11-29 14:34:52 -0800

    Add likes and comments to Post model

--- models/Post.js
+++ models/Post.js
@@ -11,7 +11,46 @@ const PostSchema = new Schema({
     type: String,
     required: true
   },
-  
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      } 
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 Profile = mongoose.model("post", PostSchema);
